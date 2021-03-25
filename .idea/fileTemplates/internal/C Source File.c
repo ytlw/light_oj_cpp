6e540609a3fbe8caaee8a06069ad805cbd6ca052
杨明航 <yangminghang@bytedance.com>
2021-03-25 23:01:24 +0800

    init

---
+++ .idea/fileTemplates/internal/C Source File.c
@@ -0,0 +1,28 @@
+#[[#include]]# <iostream>
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+#ifdef INPUT_FILE
+    FILE *fre = freopen(INPUT_FILE, "r", stdin);
+    if (fre == nullptr) {
+        cout << "invalid input file" << endl;
+        return 0;
+    }
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        cout << "Case " << i << ": ";
+        solve();
+    }
+#ifdef INPUT_FILE
+    fclose(fre);
+#endif
+    return 0;
+}
